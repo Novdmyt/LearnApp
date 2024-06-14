05343dabcacac1e8a3507e3e0ea1c1898516fc51
Novdmyt <135864604+Novdmyt@users.noreply.github.com>
2024-06-15 00:51:49 +0300

    Сделана автоматическая переключения расскладки

--- app/src/main/java/com/android/learnapp/lang/Language.java
+++ app/src/main/java/com/android/learnapp/lang/Language.java
@@ -1,10 +1,14 @@
 package com.android.learnapp.lang;
 
 import android.content.Context;
+import android.os.LocaleList;
 import android.view.MenuInflater;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.EditText;
 import android.widget.PopupMenu;
 import android.widget.Spinner;
+import android.widget.TextView;
 
 import com.android.learnapp.R;
 
@@ -15,7 +19,7 @@ public class Language {
         this.context = context;
     }
 
-    public void showWordLanguageMenu(View v, Spinner spinner) {
+    public void showWordLanguageMenu(View v, Spinner spinner, EditText editText) {
         PopupMenu popupMenu = new PopupMenu(context, v);
         MenuInflater inflater = popupMenu.getMenuInflater();
         inflater.inflate(R.menu.language_word, popupMenu.getMenu());
@@ -23,15 +27,17 @@ public class Language {
             int itemId = item.getItemId();
             if (itemId == R.id.language_english) {
                 spinner.setSelection(0); // Assuming 0 is the position for English in the spinner
+                setLanguageInputType(editText, "en");
             } else if (itemId == R.id.language_german) {
                 spinner.setSelection(1); // Assuming 1 is the position for German in the spinner
+                setLanguageInputType(editText, "de");
             }
-            return false;
+            return true;
         });
         popupMenu.show();
     }
 
-    public void showTranslationLanguageMenu(View v, Spinner spinner) {
+    public void showTranslationLanguageMenu(View v, Spinner spinner, EditText editText) {
         PopupMenu popupMenu = new PopupMenu(context, v);
         MenuInflater inflater = popupMenu.getMenuInflater();
         inflater.inflate(R.menu.language_translation, popupMenu.getMenu());
@@ -39,11 +45,22 @@ public class Language {
             int itemId = item.getItemId();
             if (itemId == R.id.language_ukrainian) {
                 spinner.setSelection(0); // Assuming 0 is the position for Ukrainian in the spinner
+                setLanguageInputType(editText, "uk");
             } else if (itemId == R.id.language_russian) {
                 spinner.setSelection(1); // Assuming 1 is the position for Russian in the spinner
+                setLanguageInputType(editText, "ru");
             }
-            return false;
+            return true;
         });
         popupMenu.show();
+    }
+
+    private void setLanguageInputType(EditText editText, String languageCode) {
+        InputMethodManager imm = (InputMethodManager) context.getSystemService(Context.INPUT_METHOD_SERVICE);
+        if (imm != null) {
+            LocaleList localeList = LocaleList.forLanguageTags(languageCode);
+            editText.setImeHintLocales(localeList);
+            imm.restartInput(editText);
+        }
     }
 }
